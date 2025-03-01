c200a92fa3eae597a71443dbb0102aeadeb5d642
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2025-02-28 20:16:05 -0400

    Create MRFC_codigo_base.py

---
+++ micropython_exercicios/MRFC_codigo_base.py
@@ -0,0 +1,20 @@
+#Codigo base do mrfc
+
+from mfrc522 import MFRC522
+from machine import Pin
+from machine import SPI
+import time
+
+
+spi = SPI(2, baudrate=2500000, polarity=0, phase=0)
+spi.init()
+rdr = MFRC522(spi=spi, gpioRst=4, gpioCs=5)
+
+while True:
+    (stat, tag_type) = rdr.request(rdr.REQIDL)
+    if stat == rdr.OK:
+        (stat, raw_uid) = rdr.anticoll()
+        if stat == rdr.OK:
+            card_id = "0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+   
+            print(card_id)
