b52797e2acc62621a82cc2aa63b756c27df95a0d
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2025-03-11 20:32:33 -0400

    Create main.py

---
+++ http_mqtt/main.py
@@ -0,0 +1,13 @@
+#> Thonny > Options > Package manager... > uresquest
+
+
+import urequests
+import time
+
+url = "http://10.31.2.83:5000/tasks"
+
+response = urequests.get(url)
+
+if response.status_code == 200:
+    print("Tarefas recebidas com sucesso!")
+    print (response.json())
